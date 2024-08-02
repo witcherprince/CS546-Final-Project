f00dbb5841f24f462b68192bc38ebeb5e595eff2
cgranger29 <cgranger29@gmail.com>
2024-08-02 14:51:33 -0400

    made routes make more logical sense

--- app.js
+++ app.js
@@ -34,14 +34,11 @@ app.use("/login", (req, res, next) => {
   // const expiresAt = new Date();
   // expiresAt.setHours(expiresAt.getHours() + 1);
   // res.cookie("loginCookie", "testValue", { expires: expiresAt });
-  console.log("hitting login middleware");
   if (req.session.user) {
     console.log("GOING TO USERS");
-    return res.redirect("/users");
-  } else {
-    // req.method = "POST";
-    next();
+    return res.redirect("/users/userPage");
   }
+  next();
 });
 
 configRoutesFunction(app);
--- routes/users.js
+++ routes/users.js
@@ -3,35 +3,8 @@ import userValidations from "../data/users.js";
 
 const router = express.Router();
 
-router.route("/").get(async (req, res) => {
+router.route("/userPage").get(async (req, res) => {
   return res.render("users/userPage");
-});
-
-router.route("/userPage").post(async (req, res) => {
-  console.log(req.body);
-
-  if (!req.body["emailaddress"]) {
-    return res.status(400).send("Must provide an email address");
-  }
-  if (!req.body["passwordInput"]) {
-    return res.status(400).send("Must provide a password");
-  }
-  const emailAddressFormatted = req.body["emailaddress"];
-  const password = req.body["passwordInput"];
-
-  req.session.user = {
-    emailAddress: emailAddressFormatted,
-    loggedIn: true,
-  };
-
-  try {
-    await userValidations.loginUser(emailAddressFormatted, password);
-    return res.render("users/userPage");
-  } catch ({ name, message }) {
-    return res
-      .status(400)
-      .render("users/loginUserErrors", { errorMessage: message });
-  }
 });
 
 export default router;
