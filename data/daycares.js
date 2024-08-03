0de7c5756e9fe9355460300c0d1acd1a59d5a165
Fez90 <93689343+Fez90@users.noreply.github.com>
2024-08-02 22:43:39 -0400

    some modifications

--- data/daycares.js
+++ data/daycares.js
@@ -142,24 +142,24 @@ export const getAll = async () => {
   };
 
 // 5. Get daycare by name from database
-export const getOrg = async (name) => {
+export const getOrg = async (id) => {
 
-    if (!name) {
-      throw 'You must provide an name of day organization to search for'
+    if (!id) {
+      throw 'You must provide id of day organization to search for'
     };
   
-    if (typeof name !== 'string') {
+    if (typeof id !== 'string') {
       throw 'Id must be a string';
     } 
   
-    if (name.trim().length === 0) {
+    if (id.trim().length === 0) {
       throw 'Name of day organization cannot be an empty string or just spaces';
     }
   
-    name = name.trim();
+    id = id.trim();
   
     const dayCaresCollection = await daycares();
-    const dayCare = await dayCaresCollection.findOne({ name: name });
+    const dayCare = await dayCaresCollection.findOne({ _id: new ObjectId(id) });
   
     if (dayCare === null) {
       throw 'No day organization with that name';
@@ -171,4 +171,108 @@ export const getOrg = async (name) => {
   
   };
 
+// modified add daycare function
+export const addDayCare = async(
+    name, //Requied
+    introduction, //requied
+    location, //required
+    contactInfo, //required
+    businessHours, //requied
+    tuitionRange, //not-requied, but recommend
+    availability, //not-requied, but recommend
+    yearsInBusiness, //not-requied
+    lunchChoices, //not-requied
+    duration //not-requied 
+) => {
+    //input checking: ...
+    name = validation.checkString(name, 'name');
+    introduction = validation.checkString(introduction, 'introduction');
+    location = validation.isValidObject(location, 'location');
+    businessHours = validation.checkBusinessHour(businessHours);
+    contactInfo = validation.isValidObject(contactInfo, 'contactInfo');
 
+    let address = location.address;
+    let town = location.town;
+    let state = location.state;
+    let zipcode = location.zipcode;
+    let email = contactInfo.email;
+    let phone = contactInfo.phone;
+    let website = contactInfo.website;
+
+    if (!address || !town || !state || !zipcode) {
+        throw 'Location must have address, town, state and zipcode';
+    }
+
+    address = validation.checkString(address, 'address');
+    town = validation.checkString(town, 'town')
+    state = validation.checkState(state);
+    zipcode = validation.checkZipcode(zipcode);
+    email = validation.checkEmail(email);
+    phone = validation.checkPhone(phone);
+
+
+    if (website) {
+        website = validation.checkWebsite(website);
+    } else {
+        website = NULL;
+    }
+
+    if (yearsInBusiness) {
+        yearsInBusiness = validation.checkNumber(yearsInBusiness, 'yearsInBusiness');
+    } else {
+        yearsInBusiness = NULL;
+    }
+
+    if (availability) {
+        availability = validation.checkBoolean(availability, 'availability')
+    } else {
+        availability = NULL;
+    }
+
+    if (lunchChoices) {
+        for (let i = 0; i < lunchChoices.length; i++) {
+            lunchChoices[i] = validation.checkString(lunchChoices[i], 'lunchChoices');
+        }
+    } else {
+        lunchChoices = NULL;
+    }
+
+    //Adding the daycare:
+    let newDaycare = {
+        name: name,
+        introduction: introduction,
+        location:{
+            address: address,
+            town: town,
+            state: state,
+            zipcode: zipcode
+        },
+        contactInfo: {
+            email: email,
+            phone: phone,
+            website: website
+        },
+        businessHours: businessHours,
+        tuitionRange: tuitionRange,
+        availability: availability,
+        yearsInBusiness: yearsInBusiness,
+        lunchChoices: [lunchChoices],
+        duration: duration
+    }
+
+    // Inserting daycare into database
+    const dayCaresCollection = await daycares();
+
+    const insertInfo = await dayCaresCollection.insertOne(newDaycare);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+        throw 'Could not add day care organization';
+    }
+
+    const newId = insertInfo.insertedId.toString();
+    const dayCare = await getOrg(newId);
+    dayCare._id = dayCare._id.toString();
+    return dayCare;
+
+}
+
+  
