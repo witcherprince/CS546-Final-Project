e1c426f5370abf9ae60b5d20b318f6a50ee23e89
Fez90 <93689343+Fez90@users.noreply.github.com>
2024-08-18 11:30:52 -0400

    fixed form did not submit before

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -4,7 +4,9 @@ const password = document.getElementById('passwordInput');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  validateInputs();
+  if (validateInputs()) {
+    form.submit();
+  }
 
 });
 
@@ -26,24 +28,31 @@ const setSuccess = element => {
 }
 
 const validateInputs = () => {
+  let valid = true;
   const emailVal = email.value.trim();
   const passwordVal = password.value.trim();
 
   if(emailVal === '') {
     setError(email,'Email is required');
+    valid = false;
   } else if(!checkEmail(emailVal)) {
-    setError(email, 'Invalid email,should include @ character and end with .com');
+    setError(email, 'Invalid email format');
+    valid = false;
   } else {
     setSuccess(email);
   }
 
   if(passwordVal === '') {
     setError(password,'Password is required');
-  } else if(!isValidPassword(passwordVal)) {
+    valid = false
+  } else if(!checkPassword(passwordVal)) {
     setError(password, 'Invalid password,should include 8 characters, one special and one uppercase');
+    valid = false;
   } else {
     setSuccess(email);
   }
+
+  return valid;
 };
 
 function checkEmail(email) {
@@ -55,4 +64,27 @@ function checkEmail(email) {
 function isValidPassword(input) {
   let re = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+[\]{};':",.<>?/\\|`~]).{8,}$/;
   return re.test(input);
+}
+
+function checkPassword(val) {
+  if (!val) return false;
+  if (typeof val !== "string") return false;
+  val = val.trim();
+  if (val.length === 0) {
+    return false;
+  }
+  if (val.length < 8) {
+    return false
+  }
+  // Other types of checks
+  if (!/[A-Z]/.test(val)) {
+    return false;
+  }
+  if (!/[0-9]/.test(val)) {
+    return false;
+  }
+  if (!/[^a-zA-Z0-9]/.test(val)) {
+    return false;
+  }
+  return true;
 }
